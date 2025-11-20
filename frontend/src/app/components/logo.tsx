9dc59cda345a6d7474b97e67e5761add075f3264
xupie <example@example.com>
2025-11-20 13:06:47 +0200

    icons

--- frontend/src/app/components/logo.tsx
+++ frontend/src/app/components/logo.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export function HamburgerMenu() {
   return (
     <Image
-      src={`/hamburger-menu/hamburger-menu.svg`}
+      src={`/icons/hamburger-menu/hamburger-menu.svg`}
       alt={"icon of mobile navigation menu"}
       width={36}
       height={36}
