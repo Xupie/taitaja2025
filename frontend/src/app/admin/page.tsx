8ebe7908a8f6d87744906b6380a3629528bcc55a
xupie <example@example.com>
2025-11-20 10:24:37 +0200

    structure

---
+++ frontend/src/app/admin/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Admin() {
+  return (
+    <main>
+        <h1>hello world</h1>
+    </main>
+  );
+}
---
+++ frontend/src/app/game/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Game() {
+  return (
+    <main>
+        <h1>hello world</h1>
+    </main>
+  );
+}
